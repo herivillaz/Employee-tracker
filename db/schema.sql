b8a98f518e342fa188a38d526ee0fd978fc66417
herivillaz <herivillaz@gmail.com>
2020-08-01 13:48:35 -0400

    fix bug on schema

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE role (
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
   INDEX dep_ind (department_id),
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
 PRIMARY KEY (id)
 );
 
